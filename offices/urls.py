030444dc91aa25a3ba775c39e814cb9fc72730e2
YetundeAlabi <alabiyetunde007@gmail.com>
2023-07-12 12:00:03 +0100

    run migrations

--- offices/urls.py
+++ offices/urls.py
@@ -7,7 +7,7 @@ app_name = "offices"
 urlpatterns = [
     path("office/countries", views.CountryListAPIView.as_view(), name="countries"),
     path("office/countries/<int:country_pk>/cities", views.CityListCreateAPIView.as_view(), name="cities"),
-    path("offices/create/", views.LocationListCreateAPIView.as_view(), name="list_create_location"),
+    path("offices/create", views.LocationListCreateAPIView.as_view(), name="list_create_location"),
     path("offices/<int:pk>/update", views.LocationUpdateAPIView.as_view(), name="location_update"),
     path("offices/<int:pk>/delete", views.LocationDestroyAPIView.as_view(), name="delete_location"),
 ]
--- staff_mgt/urls.py
+++ staff_mgt/urls.py
@@ -5,10 +5,10 @@ from staff_mgt import views
 app_name = "staff_mgt"
 urlpatterns = [
     path("dashboard/", views.DashboardAPIView.as_view(), name="dashboard"),
-    path("staff/create", views.StaffCreateAPIView.as_view(), name="create_staff"),
-    path("staff/", views.StaffListAPIView.as_view(), name="staff_list"),
-    path("staff/<int:pk>/", views.StaffRetrieveUpdateAPIView.as_view(), name="staff_retrieve_update"),
-    path("staff/<int:pk>/suspend", views.SuspendStaffAPIView.as_view(), name="suspend_staff"),
-    path("staff/export", views.ExportStaffAPIView.as_view(), name="staff_export"),
+    path("create", views.StaffCreateAPIView.as_view(), name="create_staff"),
+    path("", views.StaffListAPIView.as_view(), name="staff_list"),
+    path("<int:pk>/", views.StaffRetrieveUpdateAPIView.as_view(), name="staff_retrieve_update"),
+    path("<int:pk>/suspend", views.SuspendStaffAPIView.as_view(), name="suspend_staff"),
+    path("export", views.ExportStaffAPIView.as_view(), name="staff_export"),
     path("admin/<int:pk>/", views.AdminDetailAPIView.as_view(), name="admin-detail")
 ]
