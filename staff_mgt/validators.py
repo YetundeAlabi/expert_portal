07a6ac00975c5fdf86a649392f31ee2d8e78aa43
Yetunde Alabi <alabiyetunde007@gmail.com>
2023-07-15 16:27:58 +0100

    Update validators.py

--- staff_mgt/validators.py
+++ staff_mgt/validators.py
@@ -6,7 +6,7 @@ from rest_framework.validators import UniqueValidator
 from .models import Staff
 
 def validate_email_domain(value):
-    valid_email_domain = ("afexnigeria.com", "africaexchange.com", "afexkenya.com", "afexuganda.com", "afex.africa")
+    valid_email_domain = ("gmail.com","afexnigeria.com", "africaexchange.com", "afexkenya.com", "afexuganda.com", "afex.africa")
     if not value.endswith(valid_email_domain):
         raise serializers.ValidationError("Incorrect credentials.")
     return value
